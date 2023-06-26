b3fa347f6f73f02e63c5247a9b485e34f506f11d
Denis Kulik <kulikdenis1996x@gmail.com>
2023-06-26 16:23:12 +0700

    fix text messages and info

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -30,31 +30,29 @@ const HW13 = () => {
             .then(() => {
                 setCode('Код 200!');
                 setImage(success200);
-                setText('...всё ок)\n' +
-                    'код 200 - обычно означает что скорее всего всё ок)');
-                setInfo('');
+                setText('...всё ок)');
+                setInfo('код 200 - обычно означает что скорее всего всё ок)');
             })
             .catch((e) => {
-                setInfo('');
-
                 switch (e.response.status) {
                     case 400:
                         setCode('Ошибка 400!');
                         setImage(error400);
-                        setText('Ты не отправил success в body вообще!\n' +
+                        setText('Ты не отправил success в body вообще!');
+                        setInfo(
                             'ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!');
                         break;
                     case 500:
                         setCode('Ошибка 500!');
                         setImage(error500);
-                        setText('эмитация ошибки на сервере\n' +
+                        setText('эмитация ошибки на сервере');
+                        setInfo(
                             'ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)');
                         break;
                     case 0:
-                        setInfo('Error!');
+                        setCode('Error!');
                         setImage(errorUnknown);
-                        setText(`Network Error 
-                        AxiosError`);
+                        setInfo(`Network Error AxiosError`);
                         break;
                 }
             });
@@ -71,7 +69,7 @@ const HW13 = () => {
                         onClick={send(true)}
                         xType={'secondary'}
                         // дописать
-                        disabled={!!info}
+                        disabled={info === '...loading'}
                     >
                         Send true
                     </SuperButton>
@@ -79,7 +77,7 @@ const HW13 = () => {
                         id={'hw13-send-false'}
                         onClick={send(false)}
                         xType={'secondary'}
-                        disabled={!!info}
+                        disabled={info === '...loading'}
                     >
                         Send false
                     </SuperButton>
@@ -87,7 +85,7 @@ const HW13 = () => {
                         id={'hw13-send-undefined'}
                         onClick={send(undefined)}
                         xType={'secondary'}
-                        disabled={!!info}
+                        disabled={info === '...loading'}
                     >
                         Send undefined
                     </SuperButton>
@@ -95,7 +93,7 @@ const HW13 = () => {
                         id={'hw13-send-null'}
                         onClick={send(null)}
                         xType={'secondary'}
-                        disabled={!!info}
+                        disabled={info === '...loading'}
                     >
                         Send null
                     </SuperButton>
